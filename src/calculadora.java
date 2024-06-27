da2b5433523ca75da86f496279707f4d24f150ff
Bradin <danbleeding123@gmail.com>
2024-06-26 23:34:18 -0300

    first commit

---
+++ src/calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação deste classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Dan Rabadji
+ * @version 1.0
+ * @since 26/06/2024
+ */
+public class calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ src/comentarios.java
@@ -0,0 +1,22 @@
+public class comentarios {
+    public static void main(String[] args) {
+        //Olá, eu sou um comentário em uma única linha
+        /*
+         * Olá
+         * Eu sou um comentário
+         * e escrito desta forma
+         * eu posso ser mais detalhado
+         * quando necessário
+         */
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * são para identificar que você
+     * pretende elaborar um comentário
+     * de documentação.
+     * Incrível!
+     */
+        public void metodo(){
+            //comentários não devem ser usados para mitigar um código defeituoso.
+        }
+}
